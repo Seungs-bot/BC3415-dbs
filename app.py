18d5646836e247f966f8259f395c1a742b8d6c3d
Seungs <leeseungsoo1999@gmail.com>
2024-01-23 12:23:57 +0800

    final commit

--- app.py
+++ app.py
@@ -3,7 +3,8 @@ app = Flask(__name__)
 @app.route("/",methods=["GET","POST"])
 def index():
     if request.method == "POST":
-        return(render_template("index.html",result="no model yet.........."))
+        rate = float(request.form.get("rate"))
+        return(render_template("index.html",result=(rate * -50.6) + 90.2))
     else:
         return(render_template("index.html",result="waiting for rate..........."))
 if __name__ == "__main__":
